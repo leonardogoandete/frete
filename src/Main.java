369c8da28ae60cc5e44ce87223502e42113b3a54
leonardo_goandete <leonardo_goandete@sicredi.com.br>
2023-04-14 23:07:05 -0300

    Implementado classe CLiente e Pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import br.com.ifrs.frete.pessoas.Cliente;
+import br.com.ifrs.frete.pessoas.Pessoa;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Pessoa p1 = new Pessoa("Leo");
+
+
+        System.out.println(p1.toString());
+
+        Cliente cli = new Cliente("Luna","Trav. Beco dos Papagaio 12","51999999999","99933387234",122);
+        System.out.println(cli.toString());
     }
 }
---
+++ src/br/com/ifrs/frete/pessoas/Pessoa.java
@@ -0,0 +1,22 @@
+package br.com.ifrs.frete.pessoas;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString(){
+        return getNome();
+    }
+}
